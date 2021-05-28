dd9794a0e6553e0ae2712c791829446022491aeb
Leonardo Cavaletti <leon@random42.com>
2021-05-28 21:12:44 +0100

    Improved styling, started writing sample page

--- editor/index.js
+++ editor/index.js
@@ -1,9 +1,14 @@
 import "./style.css";
+import "../lib/style.css";
 
-import test from "./test.txt?raw";
-import { preprocess, transform, process, render } from "../lib/imml";
+import test from "./test.imml?raw";
+import { preprocess, transform, process, render, parse } from "../lib/imml";
 
 const nodes = preprocess(test);
+console.log(nodes);
 const tokens = transform(nodes);
+console.log(tokens)
 const data = process(tokens);
+console.log(data)
+// const data = parse(test);
 render(data, document);
